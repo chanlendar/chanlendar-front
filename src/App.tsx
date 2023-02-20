6b8d569c812ac9cbd2359be87193902cbbe161ea
Dellil <aksaksdm@naver.com>
2023-02-20 14:35:37 +0900

    chore: GlobalStyle 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import styled from "@emotion/styled";
-import { observer, useLocalObservable, useAsObservableSource } from "mobx-react-lite";
-import { useState } from "react";
-
-const TimerView = observer(() => {
-	const timer = useLocalObservable(() => ({
-		secondsPassed: 0,
-		increaseTimer() {
-			this.secondsPassed++;
-		},
-	}));
-
-	return (
-		<span>
-			Seconds passed: {timer.secondsPassed}{" "}
-			<button onClick={timer.increaseTimer}>click here!</button>
-		</span>
-	);
-});
-const Div = styled.div`
-	color: blue;
-`;
+import GlobalStyles from "@/GlobalStyles";
 
 function App() {
 	return (
-		<Div>
-			Electron + vite + react + emotion + storybook
-			<TimerView />
-		</Div>
+		<>
+			<GlobalStyles />
+			<div>asdfasf</div>
+		</>
 	);
 }
 
