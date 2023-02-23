07b4fd91494cee9763ba7831bb40042a18f19370
Dellil <aksaksdm@naver.com>
2023-02-23 14:29:49 +0900

    fix: Page 컴포넌트 배경 1px 밀림 현상 수정

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 const Page = styled.div`
-	height: calc(100% - 57px);
+	height: calc(100% - 56px);
 	background-color: ${({ theme }) => theme.body.backgroundColor};
 `;
 
