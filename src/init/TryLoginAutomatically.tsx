a5200aa95a768f7447f3a06f9ec6679aa82e75aa
Dellil <aksaksdm@naver.com>
2023-02-26 20:37:07 +0900

    add: 자동 로그인시 쿠키 maxAge 갱신

--- src/init/TryLoginAutomatically.tsx
+++ src/init/TryLoginAutomatically.tsx
@@ -6,13 +6,28 @@ import useStores from "@/hooks/useStore";
 interface Props {}
 
 const TryLoginAutomatically: React.FC<Props> = () => {
-	const [cookies] = useCookies(["user"]);
+	const [cookies, setCookie] = useCookies(["user"]);
 	const { profileStore } = useStores();
 
 	useEffect(() => {
 		if (cookies["user"]) {
 			const { email, name, uid } = cookies["user"];
 			profileStore.setProfile(email, name, uid);
+
+			setCookie(
+				"user",
+				{
+					email,
+					name,
+					uid,
+				},
+				{
+					path: "/",
+					sameSite: "strict",
+					secure: true,
+					maxAge: 3600 * 24,
+				},
+			);
 		}
 	}, []);
 
