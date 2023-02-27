22e97df3c7724681ecce757c046fe80ae28277b2
Dellil <aksaksdm@naver.com>
2023-02-27 14:03:00 +0900

    fix: 비유저 상태 요구 hoc 로직 수정

--- src/init/RequireNonUser.tsx
+++ src/init/RequireNonUser.tsx
@@ -1,20 +1,12 @@
 import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 
-import { validateNull } from "@/utils";
-
 interface Props {}
 
 const RequireNonUser = ({ children }: React.PropsWithChildren<Props>) => {
 	const [cookies] = useCookies(["user"]);
 
 	if (cookies["user"]) {
-		return <Navigate to="/" replace />;
-	}
-
-	const { email, name, uid } = cookies["user"];
-
-	if (!validateNull(email, name, uid)) {
 		return <Navigate to="/" replace />;
 	}
 
